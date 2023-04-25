19ce31543b9aeb3c799241d37406736810f93118
luis <luchosaque@gmail.com>
2023-04-25 21:50:57 +0200

    primer commit

---
+++ src/main/java/com/HelloController.java
@@ -0,0 +1,9 @@
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@GetMapping("/hello")
+    public String hello() 
+    {
+        return "Hello luis";
+    }
